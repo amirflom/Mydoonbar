cf564bf5e1723a09cd7a3547587ea8e192afb540
amirflom <amyrsharifian@gmail.com>
2025-11-29 20:18:21 +0330

    add home page for render

--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+        return HttpResponse("Mydoonbar is Running Successfully!")
+
 
 urlpatterns = [
+    path('', home)
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
